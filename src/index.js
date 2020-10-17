32ee4db30c5d74c730590995d9125d9c6b796e45
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2020-10-17 23:49:16 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+	var n1 = 0;
+	if(n<0){
+		n = n * -1;
+	}
+	
+  while(n>0){
+  	n1 = n1 *10;
+  	n1 = n1 + n % 10;
+  	n = Math.floor(n / 10);
+
+  }
+  return(n1);
 }
